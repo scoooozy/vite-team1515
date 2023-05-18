3c2d46045c3f7e7c84a4362a42b374cca7de74fa
Scoozy <79184720+scoooozy@users.noreply.github.com>
2023-05-18 11:46:04 -0700

    i hate media querys

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import "../Footer/Footer.css";
 
-import InstagramIcon from '@mui/icons-material/Instagram';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 function Footer(props) {
   return (
@@ -19,9 +19,17 @@ function Footer(props) {
         )}
         <Row className="footer-row">
           <Col className="footer-info">
-            <a href="https://www.instagram.com/frc1515/?hl=en"><InstagramIcon/></a>
-            <a href="https://www.facebook.com/frc1515/"><FacebookIcon/></a>
-            <a href="https://twitter.com/frc1515?lang=en"><TwitterIcon/></a>
+            <p className="infos">Follow and support us!!!</p>
+            <span>|</span>  
+            <a href="https://www.instagram.com/frc1515/?hl=en">
+              <InstagramIcon />
+            </a>
+            <a href="https://www.facebook.com/frc1515/">
+              <FacebookIcon />
+            </a>
+            <a href="https://twitter.com/frc1515?lang=en">
+              <TwitterIcon />
+            </a>
           </Col>
         </Row>
       </Container>
