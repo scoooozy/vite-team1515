c6e34c7055e39f01ed6de14bf91c9bd73a6ba6ab
Scoozy <79184720+scoooozy@users.noreply.github.com>
2023-12-13 17:16:32 -0800

    new jeans

--- src/components/LeadersPage/LeadersPage.jsx
+++ src/components/LeadersPage/LeadersPage.jsx
@@ -6,14 +6,18 @@ import PersonCard from "../PersonCard/PersonCard";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Cameron from "./cameron_1500x1000.webp";
-import Andrew from "./andrew_1500x1000.webp";
-import Chazz from "./chazz_1500x1000.webp";
-import Kaylen from "./kaylen_1500x1000.webp";
-import Odelia from "./odelia_1500x1000.webp";
-import Rachel from "./rachel_1500x1000.webp";
-import Arielle from "./ariella_1500x1000.webp";
-import Castle from "./castle_1500x1000.webp";
+import gad from "./assets/gad.webp";
+import hay from "./assets/hay.webp";
+import jus from "./assets/jus.webp";
+import kay from "./assets/kay.webp";
+import ode from "./assets/ode.webp";
+import rac from "./assets/rac.webp"
+import ari from "./assets/ari.webp";
+import mat from "./assets/mat.webp";
+import yur from "./assets/yur.webp";
+
+
+import Castle from "./assets/castle.webp";
 import LeadersBG from "./leadership.webp";
 import Footer from "../Footer/Footer";
 import { useRef } from "react";
@@ -37,47 +41,48 @@ const LeadersPage = (props) => {
         <Row>
           <PersonCard
             ref={ref}
-            bg={Andrew}
-            name="Andrew Galen"
+            bg={ode}
+            name="Odelia Shofet"
             position="President"
-            description="I am currently a Senior and am on my fourth year on the robotics team. For the past couple years I have been a programming specialist developing code for the robot and teaching new students. I now am the President of the team, where I help lead multiple divisions and set a model for newer members. I enjoy working with such an amazing group of students and cannot wait to have an another great year with Mortorq."
+            description="I am a current senior and have been on this team for the past four years. As machine specialist, I oversee the woodshop and teach new members about the machinery. From freshman year, I have loved everything mechanical and machinery. Being on MorTorq, I have learned a variety of skills, from using SolidWorks to leading a group of students. I look forward to the rest of the year and next year on this team! Go MorTorq!"
           />
           <PersonCard
-            bg={Cameron}
-            name="Cameron Asher"
-            position="Vice President"
-            description="This is my fourth year on the robotics team. I'm currently the vice president, head of programming, and the head of web development I've loved programming for a while and being on Mortorq has taught me many practical and real-world skills so I'm very grateful for FRC and Mortorq. I really enjoy leading the amazing students on our team and hope to help make this team the best it can."
+            bg={kay}
+            name="Kaylen Wi"
+            position="Co-President"
+            description="This is my fourth year on the team. As electrical specialist, I wire the robot to ensure Build can run the different components they built with the code Programers wrote. Electrical piqued my interest freshman year and since then I worked very hard to increase my knowledge. Mortorq has given me many opportunities from learning how to use power tools to developing my engineering and leadership skills. I have met so many amazing people through this program and hope to encourage more girls to join FIRST."
           />{" "}
           <PersonCard
-            bg={Chazz}
-            name="Chazz Rugiero"
+            bg={jus}
+            name="Justin Perlman"
             position="Build Specialist"
-            description="This is my 4th year on MorTorq and in FRC, and I am a senior. Previously, I was head pit tech. As build leader I ensure that all mechanical aspects of the robotare in check, and that all build sub-teams are working in cohesion. This team is like my second family, and I am so grateful to be able to work alongside them. My favorite parts of FRC are the people involved and their robots. The diversity involved in both the people competing and their design choices is beautiful. <3"
+            description="This is my 4th year at Mortorq and taking part in FRC! For the past few years, I’ve been the team’s driver. This year I’m the Build Specialist! It's my duty to teach engineering to our new members, make sure our robot is designed and built with high quality, and keep our build subgroups operating cohesively together! I absolutely love being a part of this team. Everybody has such a positive, inclusive attitude and I couldn't feel more at home. I can't wait to see what we create next! Go Mortorq!"
           />{" "}
         </Row>
         <Row>
           <PersonCard
-            bg={Odelia}
-            name="Odelia Shofet"
-            position="Machine Specialist"
-            description="I am a current junior and have been on this team for the past three years. As machine specialist, I oversee the woodshop and teach new members about the machinery. From freshman year, I have loved everything mechanical and machinery. Being on MorTorq, I have learned a variety of skills, from using SolidWorks to leading a group of students. I look forward to the rest of the year and next year on this team! Go MorTorq!"
+            bg={hay}
+            name="Hayley Chechel"
+            position="Programing Specialist"
+            description="I'm the head of programming on team 1515. My job is to program the robot to complete various challenges and help train the next generation of programmers. As a junior, this is my 3rd year on this incredible team. Mortorq has taught me so much about coding and leadership and I'm so excited to help pass the knowledge down. This team has helped prepare me for real life challenges I'll see as a CS major and I'm so grateful for the experiences and lessons I've learned. I'm looking forward to another amazing year. Go Mortorq!"
           />
           <PersonCard
-            bg={Kaylen}
-            name="Kaylen Wi"
+            bg={yur}
+            name="Yury Hasson"
             position="Electrical Specialist"
-            description="This is my third year on the team. As electrical specialist, I wire the robot to ensure Build can run the different components they built with the code Programers wrote. Electrical piqued my interest freshman year and since then I worked very hard to increase my knowledge. Mortorq has given me many opportunities from learning how to use power tools to developing my engineering and leadership skills. I have met so many amazing people through this program and hope to encourage more girls to join FIRST."
+            description="Hello, My name Is Yury, I am a junior in Highschool, and I have been on the team for 3 years.  This year I was promoted to electrical specialist, my job on the team is to wire the robot and make sure everything on the robot is wired correctly. I became an electrical specialist because my dad inspired me by teaching me many of the basics of wiring and I fell in love with it. Now with me being the electrical specialist of MorTorq and having great mentors I can pursue my knowledge in electronics and possibly pursue a career in electronics.
+            "
           />{" "}
           <PersonCard
-            bg={Arielle}
+            bg={ari}
             name="Arielle Efron"
             position="Safety Capitan"
-            description="I am a sophomore and the Safety Captain of team 1515, and this is my second year in robotics. As Safety Captain, I create and enforce protocols to ensure that the team is safe at all times. Safety is an absolute necessity and priority, and I hope to share my passion for FRC with anyone interested in educating themselves on how to further protect their team. I am also committed to encouraging more girls into joining FRC."
+            description="I am a junior and this is my third year as the Safety Captain of our robotics team. Safety is an absolute necessity and priority for us, and I hope to share my passion for safety beyond just MorTorq and with anyone interested in educating themselves on how to further protect their team!"
           />{" "}
         </Row>
         <Row>
           <PersonCard
-            bg={Rachel}
+            bg={rac}
             name="Rachel Cohen"
             position="CAD Specialist"
             description="This is my third year with Team 1515. As CAD Specialist, I teach students about Computer Aided Design (CAD) and help them develop their skillson the Solidworks program. Thanks to the Robotics Program, I have developed skills in engineering, machinery, design, and leadership. I hope to continue teaching and encouraging more females, as well as many of those in the Arts departments, to join Robotics."
